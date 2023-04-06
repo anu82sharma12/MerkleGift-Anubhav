5855d0247ca5254516cd99c37ac011349b447132
Anubhav Sharma <anu82sharma12@gmail.com>
2023-04-06 15:18:13 +0530

    First Commit

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const verifyProof = require('../utils/verifyProof');
+const niceList = require('../utils/niceList.json')
+const MerkleTree = require("../utils/MerkleTree")
+
+const port = 1225;
+
+const app = express();
+app.use(express.json());
+
+
+const merkleTree = new MerkleTree(niceList)
+const root = merkleTree.getRoot()
+
+
+const MERKLE_ROOT = root;
+
+app.post('/gift', (req, res) => {
+
+  const body = req.body;
+  const {name , proof} = body
+ 
+  
+  const isInTheList = verifyProof(proof,name, MERKLE_ROOT);
+  console.log(isInTheList)
+  if(isInTheList) {
+    res.send("Congrats! You got a toy robot!");
+  }
+  else {
+    res.send("Sorry,You are not on the list :(");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
